03784fed65e91f25c7f6625f6ab92d17281bae17
PouyaniArmin <pouyaniarmib@gmail.com>
2025-04-01 17:39:29 +0330

    create home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.mainLayout')
+@section('title', 'Home - Store')
+@section('content')
+
+  <div class="max-w-7xl mx-auto">
+    <h1 class="text-2xl font-bold mb-4">Welcome to Our Store</h1>
+    <p class="text-gray-700">Browse our collection of products...</p>
+  </div>
+
+  @endsection
